0125092e974d9c9a5e717c34226b121fa43b28f2
Kristi-12 <kmengri@unitsystem.net>
2022-07-12 09:07:42 +0000

    Changing form types in frontend

--- app-survey/src/components/Questions/Questions.js
+++ app-survey/src/components/Questions/Questions.js
@@ -1,15 +1,26 @@
-import { Button, Container , Box , Radio , RadioGroup , FormControlLabel, FormControl, FormLabel, ButtonGroup , Typography,Rating} from "@mui/material";
+import {
+  Button,
+  Box,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  ButtonGroup,
+  ToggleButtonGroup,
+  ToggleButton,
+  Typography,
+  Rating
+} from "@mui/material";
 import { useState } from "react";
 import Page from "../Page";
 import { Logout } from "../../features/app/Logout";
 
-
 const Questions = (props) => {
-  // ReactQuery getQuestions
+  // ReactQuery getQuestions by props
   console.log(props?.questions?.data?.questions);
 
-
-  // Index controlling questions one by one 
+  // Index controlling questions one by one
   const [index, setIndex] = useState(0);
   console.log(props?.questions?.data?.questions[index]);
 
@@ -34,172 +45,224 @@ const Questions = (props) => {
   //Rating value
   const [value, setValue] = useState(2);
 
-  //Radio Image value 
-  const [selectedValue, setSelectedValue] = useState("");
+  //ToggleBUtton value
+  const [alignment, setAlignment] = useState("web");
 
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
   };
 
-
   return (
-
     <>
-    <Page withPadding title={"Survey App"} actions={<Logout />}>
+      <Page withPadding title={"Survey App"} actions={<Logout />}>
+        {props?.questions?.isSuccess ? (
+          <div>
+            <FormControl sx={{ width: 1 }}>
+              <FormLabel>
+                <Box>
+                  <h1 style={{ color: "blue", textAlign: "center" }}>
+                    {props?.questions?.data?.questions?.[index].data?.Question}
+                  </h1>
+                </Box>
+              </FormLabel>
 
-          {props?.questions?.isSuccess ? (
-                  <div>
-                    
-
-                    <FormControl sx={{ width: 1 }} >
-                      <FormLabel>
-                        <Box>
-                          <h1 style={{ color: 'blue', textAlign: "center", }}
-                          >{props?.questions?.data?.questions?.[index].data?.Question}</h1>
-                        </Box>
-                      </FormLabel>
-
+              <div>
+                {(() => {
+                  if (
+                    props?.questions?.data?.questions?.[index].type === "type1"
+                  ) {
+                    return (
                       <div>
-                        {(() => {
-                                  if (props?.questions?.data?.questions?.[index].type === "type1") {
-                                    return (
-                                      <div>
-                                        <Box
-                                          sx={{
-                                            '& > legend': { mt: 2 },
-                                          }}
-                                        >
-                                          <Typography component="legend"></Typography>
-                                          <Rating
-                                            name="simple-controlled"
-                                            value={value}
-                                            onChange={(event, newValue) => {
-                                              setValue(newValue);
-                                            }}
-                                          />
-                                        </Box>
-                                      </div>
-                                    )
-                                  } else if (props?.questions?.data?.questions?.[index].type === "type2") {
-                                    return (
-                                      <div>
-                                        <RadioGroup
-                                          row
-                                          aria-labelledby="demo-row-radio-buttons-group-label"
-                                          name="row-radio-buttons-group"
-                                          
-                                        >
-                                          <FormControlLabel
-                                            value={props?.questions?.data?.questions?.[index].data?.Answers?.Answer1}
-                                            control={<Radio />}
-                                            label={props?.questions?.data?.questions?.[index].data?.Answers?.Answer1}
-                                          />
-                                          <FormControlLabel
-                                            value={props?.questions?.data?.questions?.[index].data?.Answers?.Answer2}
-                                               control={<Radio />}
-                                         label={props?.questions?.data?.questions?.[index].data?.Answers?.Answer2}
-                                          />
-                                          <FormControlLabel
-                                            value={props?.questions?.data?.questions?.[index].data?.Answers?.Answer3}
-                                            control={<Radio />}
-                                            label={props?.questions?.data?.questions?.[index].data?.Answers?.Answer3}
-                                          />
-                                          <FormControlLabel 
-                                            value={props?.questions?.data?.questions?.[index].data?.Answers?.Answer4} 
-                                            control={<Radio />} 
-                                            label={props?.questions?.data?.questions?.[index].data?.Answers?.Answer4} />
-                                        </RadioGroup>
-                                      </div>
-                                    )
-                                  } else if (props?.questions?.data?.questions?.[index].type === "type3") {
-                                    return (
-                                      <div>
-                                        <RadioGroup
-                                          row
-                                          aria-labelledby="demo-row-radio-buttons-group-label"
-                                          name="row-radio-buttons-group"
-                                          
-                                        >
-                                          <FormControlLabel
-                                            value={props?.questions?.data?.questions?.[index].data?.Answers?.Answer1}
-                                            control={<Radio />}
-                                            label={ <img
-                                              src='https://assets.reedpopcdn.com/marvels-spider-man-1533217236862.jpg/BROK/thumbnail/1200x1200/quality/100/marvels-spider-man-1533217236862.jpg'
-                                              className='img-fluid'
-                                              alt='tst'
-                                            />}
-                                          />
-                                          <FormControlLabel
-                                            value={props?.questions?.data?.questions?.[index].data?.Answers?.Answer2}
-                                            control={<Radio />}
-                                            label={ <img
-                                              src='https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2021_41/3513181/211017-superman-2x1-mn-1355.jpg'
-                                              className='img-fluid'
-                                              alt='tst'
-                                            />}
-                                          />
-                                          <FormControlLabel
-                                            value={props?.questions?.data?.questions?.[index].data?.Answers?.Answer3}
-                                            control={<Radio />}
-                                            label={ <img
-                                              src='https://upload.wikimedia.org/wikipedia/en/a/aa/Hulk_%28circa_2019%29.png'
-                                              className='img-fluid'
-                                              alt='tst'
-                                            />}
-                                          />
-                                          
-                                          <FormControlLabel 
-                                            value={props?.questions?.data?.questions?.[index].data?.Answers?.Answer4} 
-                                            control={<Radio />} 
-                                            label={ <img
-                                              src='https://e7.pngegg.com/pngimages/75/572/png-clipart-captain-america-captain-america.png'
-                                              className='img-fluid'
-                                              alt='tst'
-                                            />} />
-                                        </RadioGroup>
-                                      </div>
-                                    )
-                                  } else if (props?.questions?.data?.questions?.[index].type === "type4") {
-                                    return (
-                                      <div>otherCase</div>
-                                    )
-                                  } 
-                                }
-                          )()}
-
+                        <Box
+                          sx={{
+                            "& > legend": { mt: 2 }
+                          }}
+                        >
+                          <Typography component="legend"></Typography>
+                          <Rating
+                            name="simple-controlled"
+                            value={value}
+                            onChange={(event, newValue) => {
+                              setValue(newValue);
+                            }}
+                          />
+                        </Box>
                       </div>
-                      
-                      
-                      
-                    </FormControl>
+                    );
+                  } else if (
+                    props?.questions?.data?.questions?.[index].type === "type2"
+                  ) {
+                    return (
+                      <div>
+                        <RadioGroup
+                          row
+                          aria-labelledby="demo-row-radio-buttons-group-label"
+                          name="row-radio-buttons-group"
+                        >
+                          <FormControlLabel
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer1?.Score
+                            }
+                            control={<Radio />}
+                            label={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer1?.Answer
+                            }
+                          />
+                          <FormControlLabel
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer2?.Score
+                            }
+                            control={<Radio />}
+                            label={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer2?.Answer
+                            }
+                          />
+                          <FormControlLabel
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer3?.Score
+                            }
+                            control={<Radio />}
+                            label={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer3?.Answer
+                            }
+                          />
+                          <FormControlLabel
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer4?.Score
+                            }
+                            control={<Radio />}
+                            label={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer4?.Answer
+                            }
+                          />
+                        </RadioGroup>
+                      </div>
+                    );
+                  } else if (
+                    props?.questions?.data?.questions?.[index].type === "type3"
+                  ) {
+                    return (
+                      <div>
+                        <RadioGroup
+                          row
+                          aria-labelledby="demo-row-radio-buttons-group-label"
+                          name="row-radio-buttons-group"
+                        >
+                          <FormControlLabel
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer1?.Score
+                            }
+                            control={<Radio />}
+                            label={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer1?.Answer
+                            }
+                          />
+                          <FormControlLabel
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer2?.Score
+                            }
+                            control={<Radio />}
+                            label={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer2?.Answer
+                            }
+                          />
+                        </RadioGroup>
+                      </div>
+                    );
+                  } else if (
+                    props?.questions?.data?.questions?.[index].type === "type4"
+                  ) {
+                    return (
+                      <div>
+                        <ToggleButtonGroup
+                          color="primary"
+                          value={alignment}
+                          exclusive
+                          onChange={handleChange}
+                        >
+                          <ToggleButton
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer1?.Score
+                            }
+                          >
+                            {
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer1?.Answer
+                            }
+                          </ToggleButton>
 
-                    <div className="flex-btn" style={{paddingTop: 45,}}>
-                      <Button
-                      variant="contained"
-                      disabled={index === 0}
-                      onClick={handlePrevious}
-                      >
-                      Prev
-                      </Button>
-                      <Button variant="contained" onClick={handleNext}>
-                      Next
-                      </Button>
+                          <ToggleButton
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer2?.Score
+                            }
+                          >
+                            {
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer2?.Answer
+                            }
+                          </ToggleButton>
+                          <ToggleButton
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer3?.Score
+                            }
+                          >
+                            {
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer3?.Answer
+                            }
+                          </ToggleButton>
+                          <ToggleButton
+                            value={
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer4?.Score
+                            }
+                          >
+                            {
+                              props?.questions?.data?.questions?.[index].data
+                                ?.Answers?.Answer4?.Answer
+                            }
+                          </ToggleButton>
+                        </ToggleButtonGroup>
+                      </div>
+                    );
+                  }
+                })()}
+              </div>
+            </FormControl>
 
-                    </div>
-                  </div>
-                ) : (
-                  "loading time..."
-                )}
-
-          </Page>
-
-
-          
-
+            <div className="flex-btn" style={{ paddingTop: 45 }}>
+              <Button
+                variant="contained"
+                disabled={index === 0}
+                onClick={handlePrevious}
+              >
+                Prev
+              </Button>
+              <Button variant="contained" onClick={handleNext}>
+                Next
+              </Button>
+            </div>
+          </div>
+        ) : (
+          "loading time..."
+        )}
+      </Page>
     </>
-
-    
-      
   );
 };
 
