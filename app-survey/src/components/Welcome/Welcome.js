4a64a7219ee739ce5d1168bf782bee88426bd7f6
Eredoqo <eredoqorri96@gmail.com>
2022-07-13 09:28:27 +0000

    Welcome page edit

--- app-survey/src/components/Welcome/Welcome.js
+++ app-survey/src/components/Welcome/Welcome.js
@@ -1,4 +1,5 @@
 import { Button } from '@mui/material';
+import moment from "moment";
 
 
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
@@ -11,18 +12,22 @@ const Welcome = (props) => {
 
         <div >
 
-           <h1>Welcome to our survey </h1>
+           <h1>Welcome to our survey form</h1>
 
-                                          <h3>Please fill out this form</h3>
+                                          <h3>You are pleased to fill this form invited by Backoffice. </h3>
+                                          
 
                                           {props.survey?.isSuccess
 
                                                   ? <div>
-                                                      <p>Opens at {props.survey.data?.surveys[0].opens_at}</p> 
-                                                      <p>Closes at {props.survey.data?.surveys[0].closes_at}</p>
+                                                      <h3>You may fill this form within {
+                                          moment(props.survey.data?.surveys[0].closes_at).fromNow()     
+                                        }</h3>
                                                     </div>
 
                                                   : "loading time..."}
+
+                                                  <hr/>
                                                                                           
 
                                           <Button variant="contained" endIcon={<PlayCircleFilledWhiteIcon /> } href="/questions">
@@ -30,6 +35,7 @@ const Welcome = (props) => {
                                             Start
 
                                           </Button>
+                                          <h3>You can stop the proccess and return later filling this form using the same loging token.</h3>
 
         </div>
 
